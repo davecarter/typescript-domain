558d9284ec93c53c5ae0327f615cb4be0540518a
David Garcia <dave74@gmail.com>
2023-03-27 23:05:20 +0200

    apply code formatting

--- src/Domain/BTC/Repository/HttpRepository.ts
+++ src/Domain/BTC/Repository/HttpRepository.ts
@@ -1,29 +1,27 @@
-import axios from "axios";
-import { config } from "../../config";
-import { FromBpiApiRespToBpiVOMapper } from "../Mapper/FromBpiApiRespToBpiVOMapper";
+import axios from "axios"
+import { config } from "../../config"
+import { FromBpiApiRespToBpiVOMapper } from "../Mapper/FromBpiApiRespToBpiVOMapper"
 
 export class HttpRepository {
   static create() {
-    return new HttpRepository();
+    return new HttpRepository()
   }
 
   async getBTCPriceIndex({ fiatCurrencyCodeVO }) {
-    const { COINDESK_API_URL } = config;
-    const fiatCurrencyCodeValue = fiatCurrencyCodeVO.value();
-    const url = `${COINDESK_API_URL}${fiatCurrencyCodeValue}.json`;
-    console.log("URL", url);
+    const { COINDESK_API_URL } = config
+    const fiatCurrencyCodeValue = fiatCurrencyCodeVO.value()
+    const url = `${COINDESK_API_URL}${fiatCurrencyCodeValue}.json`
+    console.log("URL", url)
 
     try {
-      const response = await axios.get(url);
-      const mapper = FromBpiApiRespToBpiVOMapper.create();
-      const bitcoinPriceIndexVo = mapper
-        .setParams({ fiatCurrencyCodeValue })
-        .map(response.data);
+      const response = await axios.get(url)
+      const mapper = FromBpiApiRespToBpiVOMapper.create()
+      const bitcoinPriceIndexVo = mapper.setParams({ fiatCurrencyCodeValue }).map(response.data)
 
-      return bitcoinPriceIndexVo;
+      return bitcoinPriceIndexVo
     } catch (e) {
       if (e) {
-        console.error(e);
+        console.error(e)
       }
     }
   }
