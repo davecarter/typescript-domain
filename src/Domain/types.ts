472b1f478b3d880843dce835f1887555a8fd2c92
David Garcia <dave74@gmail.com>
2023-04-02 11:17:10 +0200

    FiatCurrencyValueObject type definitions

--- src/Domain/types.ts
+++ src/Domain/types.ts
@@ -1,6 +1,17 @@
 export type FiatCurrencyCode = "EUR" | "USD"
+export interface FiatCurrencyInput {
+  fiatCurrencyCode: FiatCurrencyCode;
+}
+export type ValidCodes = FiatCurrencyCode[];
+
+
 
 export interface UseCaseConstructor<T> {
   new (...args: any[]): T
   create(): T
 }
+
+
+export type ErrorMessage<T> = {
+  message: T;
+};
