05b3562fdeb4205d77b9a77c762cb7bda4934a90
David Garcia <dave74@gmail.com>
2023-04-03 08:50:28 +0200

    adds an abstract class useCase to extend from

--- src/Domain/BTC/UseCase/GetBTCPriceUseCase.ts
+++ src/Domain/BTC/UseCase/GetBTCPriceUseCase.ts
@@ -1,17 +1,19 @@
 import { HttpRepository } from "../Repository"
 import { FiatCurrencyValueObject } from "../Models"
-import { FiatCurrencyCode } from "../../types"
+import { FiatCurrencyCode, Config } from "../../types"
+import { UseCase } from "./abstract"
 
-export class GetBTCPriceUseCase {
+export class GetBTCPriceUseCase extends UseCase {
   #repository
 
   constructor({ repository }: any) {
+    super()
     if (!repository) throw new Error("Repository not provided")
     this.#repository = repository
   }
 
-  static create() {
-    const repository = HttpRepository.create()
+  static create(config: Config) {
+    const repository = HttpRepository.create(config)
     return new GetBTCPriceUseCase({ repository })
   }
 
---
+++ src/Domain/BTC/UseCase/abstract.ts
@@ -0,0 +1,5 @@
+export abstract class UseCase {
+  execute() {
+    throw new Error("Not implemented")
+  }
+}
