4cbe56e35d2f90e2c76db093e7c8019325d0c794
David Garcia <dave74@gmail.com>
2023-04-05 19:09:08 +0200

    remove unnecessary getElement function

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,10 @@ import { config } from "./Domain/config"
 type DOMElementType = Element
 
 const domain = DomainApp.create({ config })
-const priceDOMElement = document.getElementById("price");
 const appDOMElement = document.getElementById("app");
 
-function getElement(id: string): DOMElementType | null {
-  const doc = ownerDocument(document.documentElement)
-  const element = doc.getElementById(id)
-  if (!element) {
-    throw new Error(`Element with id '${id}' not found`)
-  }
-  return element
-}
-
 domain.getBTCPriceUseCase.execute({ fiatCurrencyCode: "USD" }).then((response) => {
+  const priceDOMElement = document.getElementById("price");
   console.log("price", response)
   const price = response.bpi
   priceDOMElement ? priceDOMElement.innerHTML = `<div id="price" class="btcPrice"><h1>BTC Price index</h1>${price}</div>` : null
